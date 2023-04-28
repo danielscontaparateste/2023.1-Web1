88df11fde88a752f1b41815d4c0e53720102b9b5
Daniel Ferreira <danielse@ifce.edu.br>
2023-04-28 16:09:24 -0300

    28/04

--- js_p6/js/script.js
+++ js_p6/js/script.js
@@ -11,26 +11,55 @@ function calcular(){
     }
 
     var c = "";
-    for (let k = x; k <= y; k++ ){
+    let k = x;
+    while(k <= y){
         if (k % 2 ==0){
             c = "par";
         }else{
             c = "impar";
         }
-
         txt += "<span class='"+c+"'>" + k + "</span> "; // txt = txt + " " + k;
-
-        
+        k++;
     }
+
+    // for (let k = x; k <= y; k++ ){
+    //     if (k % 2 ==0){
+    //         c = "par";
+    //     }else{
+    //         c = "impar";
+    //     }
+    //     txt += "<span class='"+c+"'>" + k + "</span> "; // txt = txt + " " + k;
+    // }
 
     console.log(txt);
 
     document.getElementById("spnRes").innerHTML = txt;
 
+    /******** Exemplos de laços infinitos  ***********/
+    // {
+    //     let k=0;    
+    //     while(true){
+    //         console.log(k++);
+    //     }
+    // } 
+    
+    // {
+    //     for(let k=0;;k++){
+    //         console.log(k);
+    //     }
+    // }
+
+    // {
+    //     for(;;){
+    //         console.log("oi");
+    //     }
+    // }
+    /******************************************/
+
 }
 
 function limpar(){
-    document.getElementById("iptX").value = document.getElementById("iptY").value = document.getElementById("spnRes").innerHTML = document.querySelector("#spnCondNum").innerHTML = "";
+    document.getElementById("iptX").value = document.getElementById("iptY").value = document.getElementById("spnRes").innerHTML = document.querySelector("#spnCondNumX").innerHTML = document.querySelector("#spnCondNumY").innerHTML = "";
     document.getElementById("iptX").className = document.getElementById("iptY").className = "iptDefault";
 }
 
@@ -38,9 +67,9 @@ function verificarX(){
     const iptX = document.getElementById("iptX");
     const x = iptX.value;   
     // document.getElementById("spnRes").innerHTML = x;
-    const spnCondNum = document.querySelector("#spnCondNum");
+    const spnCondNum = document.querySelector("#spnCondNumX");
 
-    // spnCondNum.textContent = "";
+    spnCondNum.textContent = "";
     if (x % 2 == 0){
         iptX.className = "iptPar";
         spnCondNum.textContent += " PAR ";
@@ -53,9 +82,9 @@ function verificarX(){
 function verificarY(){
     const iptY = document.getElementById("iptY");
     const y = iptY.value;   
-    const spnCondNum = document.querySelector("#spnCondNum");
+    const spnCondNum = document.querySelector("#spnCondNumY");
 
-    
+    spnCondNum.textContent = "";
     if (y % 2 == 0){
         iptY.className = " iptPar ";
         spnCondNum.textContent += " PAR ";
