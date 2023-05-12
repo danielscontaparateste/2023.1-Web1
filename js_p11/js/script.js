b452cbff4805dc59663f8ae25604e8b3c46aa13a
Daniel Ferreira <danielse@ifce.edu.br>
2023-05-12 16:20:07 -0300

    Aula 12/05

--- js_p11/js/script.js
+++ js_p11/js/script.js
@@ -1,13 +1,34 @@
 function verResposta(){
 
-    const ops = document.getElementsByName("inst");
+    var respostas = "";
 
+    // Questão 1
+    const ops = document.getElementsByName("inst");
+    const lbsOps = document.getElementsByName("lblInst");
+    
     for (let k=0; k < ops.length; k++){
         if (ops[k].checked == true){
-            console.log(ops[k].value);
+
+            // Busca o elemento HTML onde será escrito a resposta
+            respostas += "1a. Questão: "+lbsOps[k].innerHTML;
+
+            // console.log(lbsOps[k].innerHTML);
+            break;
         }
         
-    }    
+    }   
 
+    // Questão 2
+    const slcEstado = document.getElementById("slcEstado");
+    // console.log();
+    
+    respostas += "<br><br>";
+    respostas += "2a. Questão: "+slcEstado.options[slcEstado.selectedIndex].innerHTML; 
+
+
+    // Exibir as respostas na tela
+    const spnRes = document.getElementById("spnRes");
+
+    spnRes.innerHTML = respostas;
 
 }
