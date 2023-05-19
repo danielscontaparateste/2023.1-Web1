fb4e3db68d3edb4daa871df1e8c3d1bf58f60d81
Daniel Ferreira <danielse@ifce.edu.br>
2023-05-19 14:12:10 -0300

    Aula 17/05

--- js_p11/js/script.js
+++ js_p11/js/script.js
@@ -26,9 +26,53 @@ function verResposta(){
     respostas += "2a. Questão: "+slcEstado.options[slcEstado.selectedIndex].innerHTML; 
 
 
+    // Questão 3
+    const textArea = document.getElementById("ttobs");
+
+    respostas += "<br><br>";
+    respostas += "3a. Questão: "+textArea.value;
+
     // Exibir as respostas na tela
     const spnRes = document.getElementById("spnRes");
 
     spnRes.innerHTML = respostas;
+
+}
+
+let maxCaracteres = 20;
+let comecou = false;
+let intervalo;
+
+function contagem(obs,idStatus,idContagem){
+    const rotuloStatus = '#'+idStatus;
+    const rotuloContagem = '#'+idContagem;
+    const barraStatus = document.querySelector(rotuloStatus);
+    lbl = document.querySelector(rotuloContagem);
+    
+    barraStatus.value = obs.value.length;
+
+    if (!comecou){
+        comecou = true;
+        intervalo = setInterval(progride, 600);
+    }
+        
+    if (barraStatus.value >= maxCaracteres){
+        obs.readOnly = true;
+        window.clearInterval(intervalo);
+    }
+    
+    lbl.textContent = `Caracteres: ${obs.value.length}`;
+    
+}
+
+function progride(){
+    const barraProgresso = document.querySelector("#progresso");
+    barraProgresso.value = barraProgresso.value + 1;
+
+    if (barraProgresso.value >= 100){
+        const obs = document.querySelector("#ttobs");
+        obs.readOnly = true;
+        window.clearInterval(intervalo);
+    }
 
 }
